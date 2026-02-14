b0fd8e3a042847a0fde2bc1c136d91003e1fa579
luisar0602-netizen <luisar0602@gmail.com>
2026-02-14 13:57:03 -0500

    ejercicio finalizado semana 03

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -3,31 +3,43 @@ package Application;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class    Main {
+    final static int CURRENT_YEAR = 2026;
+    static List<Integer> agesStudentList = new ArrayList<>();
+    public static void main(String[] args) {
 
-    public static inc CURRENT_YEAR=2026;
+        final String GREETING_MESSAGE = "Saludo Estudiantes de Java";
+        String [] namesStudents = {"Juan", "Maria", "Pedro", "Ana", "Luis"};
+        int [] birthYears = {2000, 1998, 2002, 1995, 2001};
 
-    public static void main (String[] args) {
+        // 1. Mostramos el saludo
+        showMessage(GREETING_MESSAGE);
 
-        final String GREETING_MESSGE = "Saludo estudiantes de java";
+        // 2. Llenamos la lista de edades usando el arreglo de años
+        fillAgesList(birthYears);
 
-        String[] namesStudents = {"Juan", "Maria", "Pedro", "Ana", "Luis"};
-        int[] birthYears = {2000, 1998, 2002, 1995, 2001};
-
-        List<int[]> ageStudentList = new ArrayList<>();
-        showMessage(GREETING_MESSGE);
-
+        // 3. Mostramos la lista final combinando nombres y edades
+        listStudents(namesStudents, agesStudentList);
+    }
+    static void showMessage(String message){
+        System.out.println(message);
     }
 
-     static void showMessage(String message) {
-         System.out.println(message);
-     }
-
-     static int calculateAge (int birthYear) {
-         return CURRENT_YEAR - birthYear;
-     }
-
-
-
+    static int calculatedAge   (int birthYear) {
+        return CURRENT_YEAR - birthYear;
     }
+    static List<Integer> fillAgesList( int[] birthYears) {
+        for (int birthYear : birthYears) {
+            int age = calculatedAge(birthYear);
+            agesStudentList.add(age);
+        }
+        return agesStudentList;
+    }
+    static void listStudents(String [] namesStudents, List <Integer> agesStudents) {
+        showMessage("lista de estudiantes:");
+        for (int i = 0; i < namesStudents.length; i++){
+            showMessage(namesStudents[i] + " - Edad: " + agesStudents.get(i));
+        }
+    }
+
 }
